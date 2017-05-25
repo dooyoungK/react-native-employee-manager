f992b1ad4c95cb9620fbc420a85c4c0558d58814
Dooyoung Gi <dooyoung@favoritemedium.com>
2017-05-25 18:12:20 +0800

    Added configurations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
-
+import Config from 'react-native-config';
 import { Provider } from 'react-redux';
 import configureStore from './reducers/configureStore';
 import Router from './Router';
+
 
 const store = configureStore();
 
 
 class App extends Component {
-//    state = { loggedIn: null };
 
     componentWillMount() {
         const config = {
-            // ADD YOUR FIREBASE CONFIGURATION
+            apiKey: Config.API_KEY,
+            authDomain: Config.AUTH_DOMAIN,
+            databaseURL: Config.DB_URL,
+            projectId: Config.PROJECT_ID,
+            storageBucket: Config.STORAGE_BUCKET,
+            messagingSenderId: Config.MESSAGING_SENDER_ID
           };
 
         firebase.initializeApp(config);
-
     }
 
     render() {
